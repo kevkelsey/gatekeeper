44dd4d39843efa34abd8367df5b801b889a77012
kevkelsey <kevkelsey@gmail.com>
2017-01-08 16:37:14 -0800

    bug fixes

--- close.py
+++ close.py
@@ -7,8 +7,7 @@ import watchdog
 def close(retries=0, method = 'pi'):
     print('CLOSED FOR BUSINESS at ' + datetime.datetime.now().strftime("%m-%d %H:%M:%S"))
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(config.DOOR, GPIO.OUT)
-    GPIO.output(config.DOOR, True)
+    GPIO.setup(config.DOOR, GPIO.OUT, initial = True)
     time.sleep(1)
     GPIO.output(config.DOOR, False)
     time.sleep(config.CLOSETIME)
--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DOOR = 12
 REED = 6
 
 # Time it takes in seconds from switch push to full door closure
-CLOSETIME = 10
+CLOSETIME = 20
 RETRIES = 1
 
 # Time in seconds to wait before retrying close operation
