21e6b59ea4d2f66513a1e03f0c4676fbf252e55e
kevkelsey <kevkelsey@gmail.com>
2017-01-11 12:05:42 -0800

    test notify without opening door

--- notifytest.py
+++ notifytest.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import watchdog
+import datetime
 
 print('Running test...')
+with open(datetime.datetime.now().strftime("%Y-%m") + '.txt', 'a') as file:
+    file.write(datetime.datetime.now().strftime("%m-%d %H:%M:%S") + ' NO CHANGE testing file log\n')
 watchdog.notifyopen()
 print('Test done!')
